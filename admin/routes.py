679d6eef88789c8b7d312e77e8134890e9c6a6e6
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2025-12-04 14:18:42 +0530

    login

--- admin/routes.py
+++ admin/routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from models import AdminUser, db
+
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
-@admin_bp.route('/login',methods=['GET','POST'])
+
+@admin_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
-        username=request.form.get("username")
-        password=request.form.get("password_hash")
-        admin=AdminUser.query.filter_by(username=username).first()
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        admin = AdminUser.query.filter_by(username=username).first()
+
         if admin and admin.check_password(password):
             session["admin_id"] = admin.id
             session["admin_username"] = admin.username
@@ -15,6 +19,7 @@ def login():
             return "Invalid username or password"
 
     return render_template("admin_login.html")
+
 
 @admin_bp.route('/dashboard')
 def dashboard():
--- models.py
+++ models.py
@@ -1,17 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-db = SQLAlchemy()
 
+db = SQLAlchemy()
 
 class AdminUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
 
-
-def set_password(self, password):
+    def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-def check_password(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
