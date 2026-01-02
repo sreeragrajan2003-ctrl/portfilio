0b4c13486d49d81dd012a62bc838bb1eb5f6a013
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2026-01-02 20:38:34 +0530

    admin seed

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from models import db
 from admin.routes import admin_bp
 from viewer.routes import viewer_bp
+from werkzeug.security import generate_password_hash
+
+
 
 
 app = Flask(__name__)
@@ -13,11 +16,31 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 app.register_blueprint(admin_bp)
 app.register_blueprint(viewer_bp)
+def seed_admin():
+    from app import db
+    from models import Admin
 
+    # check if admin already exists
+    existing_admin = Admin.query.filter_by(username="Sreerag").first()
+    if existing_admin:
+        return
+
+    admin = Admin(
+        username="Sreerag",
+        password=generate_password_hash("Radhika@123")
+    )
+
+    db.session.add(admin)
+    db.session.commit()
+
+    print("Seeded default admin")
 
 
 with app.app_context():
     db.create_all()
+    from models import seed_admin
+    seed_admin()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
